5b8920d01b3561013eaba5955a4027901de72dde
Mario Lazzari <mario.lazzari@gmail.com>
2025-02-01 19:10:23 +0100

    Done

---
+++ 07_build/ex/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  mode: 'production', // Enable production mode
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all', // Enable code splitting
+    }
+  },
+  plugins: [
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+};
